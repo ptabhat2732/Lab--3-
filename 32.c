db06324e96ce6cc1f4bc04ca3c59c4bd6ff7aabc
prabhat rabari <prabhatrabari20@gmail.com>
2025-10-16 12:51:53 +0530

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n,sum=0;
+printf("Enter the value of n: ");
+scanf("%d",&n);
+
+for(int i=1; i<=n; i++)
+  {
+      sum+=2*i;
+  }
+  printf("The sum of first %d odd numbers are: %d\n",n,sum);
+return 0;
+}
